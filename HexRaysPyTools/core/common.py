7668c2d80cabe9205a6a96c016b761fedc672eb5
igogo-x86 <kirillov.ia@outlook.com>
2018-09-16 16:14:52 +0300

    fix: Improve demangled names translation

--- HexRaysPyTools/core/common.py
+++ HexRaysPyTools/core/common.py
@@ -10,11 +10,54 @@ def demangled_name_to_c_str(name):
     """
     if not BAD_C_NAME_PATTERN.findall(name):
         return name
+    idx = name.find("operator")
+    if idx >= 0:
+        idx += len("operator")
+        if name[idx:idx + 2] == "==":
+            name = name.replace("operator==", "operator_EQ_")
+        elif name[idx:idx + 2] == "!=":
+            name = name.replace("operator!=", "operator_NEQ_")
+        elif name[idx] == "=":
+            name = name.replace("operator=", "operator_ASSIGN_")
+        elif name[idx:idx + 2] == "++":
+            name = name.replace("operator++", "operator_INC_")
+        elif name[idx:idx + 2] == "--":
+            name = name.replace("operator*", "operator_PTR_")
+        elif name[idx:idx + 2] == "->":
+            name = name.replace("operator->", "operator_REF_")
+        elif name[idx] == "*":
+            name = name.replace("operator*", "operator_PTR_")
+        elif name[idx:idx + 2] == "&&":
+            name = name.replace("operator&&", "operator_LAND_")
+        elif name[idx:idx + 2] == "||":
+            name = name.replace("operator||", "operator_LOR_")
+        elif name[idx] == "!":
+            name = name.replace("operator!", "operator_LNOT_")
+        elif name[idx] == "&":
+            name = name.replace("operator&", "operator_AND_")
+        elif name[idx] == "|":
+            name = name.replace("operator|", "operator_OR_")
+        elif name[idx] == "^":
+            name = name.replace("operator^", "operator_XOR_")
+        elif name[idx:idx + 2] == "<<":
+            name = name.replace("operator<<", "operator_LEFT_SHIFT_")
+        elif name[idx:idx + 2] == ">>":
+            name = name.replace("operator>>", "operator_RIGHT_SHIFT_")
+        elif name[idx] == "+":
+            name = name.replace("operator*", "operator_ADD_")
+        elif name[idx] == "-":
+            name = name.replace("operator*", "operator_SUB_")
+        elif name[idx] == "*":
+            name = name.replace("operator*", "operator_SUB_")
+        elif name[idx] == "/":
+            name = name.replace("operator*", "operator_DIV_")
+        else:
+            raise AssertionError("Replacement of demangled string by c-string for keyword `operatorXXX` is not yet"
+                                 "implemented ({}). You can do it by yourself or create an issue".format(name))
 
     name = name.replace("~", "DESTRUCTOR_")
     name = name.replace("*", "_PTR")
     name = name.replace("<", "_t_")
     name = name.replace(">", "_t_")
-
     name = "_".join(filter(len, BAD_C_NAME_PATTERN.split(name)))
     return name
