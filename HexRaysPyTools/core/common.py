66014e87c8f83c2f97bb856c00630f0b5aec6044
igogo-x86 <i.kirillov@dsec.ru>
2018-09-18 23:31:59 +0300

    fix: Demangled name to c-string translation typos and missed operators

--- HexRaysPyTools/core/common.py
+++ HexRaysPyTools/core/common.py
@@ -19,16 +19,36 @@ def demangled_name_to_c_str(name):
             name = name.replace("operator!=", "operator_NEQ_")
         elif name[idx] == "=":
             name = name.replace("operator=", "operator_ASSIGN_")
+        elif name[idx:idx + 2] == "+=":
+            name = name.replace("operator+=", "operator_PLUS_ASSIGN_")
+        elif name[idx:idx + 2] == "-=":
+            name = name.replace("operator-=", "operator_MINUS_ASSIGN_")
+        elif name[idx:idx + 2] == "*=":
+            name = name.replace("operator*=", "operator_MUL_ASSIGN_")
+        elif name[idx:idx + 2] == "/=":
+            name = name.replace("operator/=", "operator_DIV_ASSIGN_")
+        elif name[idx:idx + 2] == "%=":
+            name = name.replace("operator%=", "operator_MODULO_DIV_ASSIGN_")
+        elif name[idx:idx + 2] == "|=":
+            name = name.replace("operator|=", "operator_OR_ASSIGN_")
+        elif name[idx:idx + 2] == "&=":
+            name = name.replace("operator&=", "operator_AND_ASSIGN_")
+        elif name[idx:idx + 2] == "^=":
+            name = name.replace("operator^=", "operator_XOR_ASSIGN_")
+        elif name[idx:idx + 3] == "<<=":
+            name = name.replace("operator<<=", "operator_LEFT_SHIFT_ASSIGN_")
+        elif name[idx:idx + 3] == ">>=":
+            name = name.replace("operator>>=", "operator_RIGHT_SHIFT_ASSIGN_")
         elif name[idx:idx + 2] == "++":
             name = name.replace("operator++", "operator_INC_")
         elif name[idx:idx + 2] == "--":
-            name = name.replace("operator*", "operator_PTR_")
+            name = name.replace("operator--", "operator_PTR_")
         elif name[idx:idx + 2] == "->":
             name = name.replace("operator->", "operator_REF_")
         elif name[idx:idx + 2] == "[]":
             name = name.replace("operator[]", "operator_IDX_")
         elif name[idx] == "*":
-            name = name.replace("operator*", "operator_PTR_")
+            name = name.replace("operator*", "operator_STAR_")
         elif name[idx:idx + 2] == "&&":
             name = name.replace("operator&&", "operator_LAND_")
         elif name[idx:idx + 2] == "||":
@@ -45,14 +65,28 @@ def demangled_name_to_c_str(name):
             name = name.replace("operator<<", "operator_LEFT_SHIFT_")
         elif name[idx:idx + 2] == ">>":
             name = name.replace("operator>>", "operator_RIGHT_SHIFT_")
+        elif name[idx:idx] == "<":
+            name = name.replace("operator<", "operator_LESS_")
+        elif name[idx:idx] == ">":
+            name = name.replace("operator>", "operator_GREATER_")
+        elif name[idx:idx + 2] == "<=":
+            name = name.replace("operator<=", "operator_LESS_EQUAL_")
+        elif name[idx:idx + 2] == ">=":
+            name = name.replace("operator>=", "operator_GREATER_EQUAL_")
         elif name[idx] == "+":
-            name = name.replace("operator*", "operator_ADD_")
+            name = name.replace("operator+", "operator_ADD_")
         elif name[idx] == "-":
-            name = name.replace("operator*", "operator_SUB_")
-        elif name[idx] == "*":
-            name = name.replace("operator*", "operator_SUB_")
+            name = name.replace("operator-", "operator_SUB_")
         elif name[idx] == "/":
-            name = name.replace("operator*", "operator_DIV_")
+            name = name.replace("operator/", "operator_DIV_")
+        elif name[idx] == "%":
+            name = name.replace("operator%", "operator_MODULO_DIV_")
+        elif name[idx:idx + 2] == "()":
+            name = name.replace("operator()", "operator_CALL_")
+        elif name[idx: idx + 6] == " new[]":
+            name = name.replace("operator new[]", "operator_NEW_ARRAY_")
+        elif name[idx: idx + 9] == " delete[]":
+            name = name.replace("operator delete[]", "operator_DELETE_ARRAY_")
         elif name[idx: idx + 4] == " new":
             name = name.replace("operator new", "operator_NEW_")
         elif name[idx: idx + 7] == " delete":
